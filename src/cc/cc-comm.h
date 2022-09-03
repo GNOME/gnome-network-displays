f308714609e6c3b6f120c850344f7f66e7a6f15e
Anupam Kumar <kyteinsky@gmail.com>
2022-09-03 16:29:59 +0530

    cc: remove redundant comments

--- src/cc/cc-comm.h
+++ src/cc/cc-comm.h
@@ -26,20 +26,6 @@
 
 G_BEGIN_DECLS
 
-// #define CC_MAX_MSG_SIZE (64 * 1024) // 64KB
-// #define CC_MAX_MESSAGE_TIMEOUT (20) // 20 seconds
-// // this might pose a problem when there are two gnd applications around
-// #define CC_DEFAULT_SENDER_ID "sender-gnd"
-// #define CC_DEFAULT_RECEIVER_ID "receiver-0"
-// #define CC_MIRRORING_APP_ID "0F5096E8"
-
-// #define CC_NAMESPACE_AUTH "urn:x-cast:com.google.cast.tp.deviceauth"
-// #define CC_NAMESPACE_CONNECTION "urn:x-cast:com.google.cast.tp.connection"
-// #define CC_NAMESPACE_HEARTBEAT "urn:x-cast:com.google.cast.tp.heartbeat"
-// #define CC_NAMESPACE_RECEIVER "urn:x-cast:com.google.cast.receiver"
-// #define CC_NAMESPACE_MEDIA "urn:x-cast:com.google.cast.media"
-// #define CC_NAMESPACE_WEBRTC "urn:x-cast:com.google.cast.webrtc"
-
 struct _CcCommClosure
 {
   gpointer userdata;
@@ -76,31 +62,6 @@ typedef enum {
   CC_MESSAGE_TYPE_WEBRTC,
 } CcMessageType;
 
-// typedef enum {
-//   CC_RWAIT_TYPE_NONE                 = 0b0,
-//   CC_RWAIT_TYPE_GET_APP_AVAILABILITY = 0b1 << 0, /* key is `responseType` */
-//   CC_RWAIT_TYPE_LAUNCH_ERROR         = 0b1 << 1, /* all other keys are `type` */
-//   CC_RWAIT_TYPE_ANSWER               = 0b1 << 2,
-//   CC_RWAIT_TYPE_RECEIVER_STATUS      = 0b1 << 3,
-//   CC_RWAIT_TYPE_MEDIA_STATUS         = 0b1 << 4,
-//   CC_RWAIT_TYPE_PING                 = 0b1 << 5,
-//   CC_RWAIT_TYPE_PONG                 = 0b1 << 6,
-//   CC_RWAIT_TYPE_CLOSE                = 0b1 << 7,
-//   CC_RWAIT_TYPE_UNKNOWN              = 0b1 << 8,
-// } CcReceivedMessageType;
-
-// // typedef CcReceivedMessageType CcWaitingFor;
-// #define CcWaitingFor CcReceivedMessageType
-
-// typedef enum {
-//   CC_WAITING_FOR_NOTHING         =  0b0,
-//   CC_WAITING_FOR_APP_AVAILABLE   =  0b1 << 0,
-//   CC_WAITING_FOR_BROADCAST       =  0b1 << 1,
-//   CC_WAITING_FOR_STATUS          =  0b1 << 2,
-//   CC_WAITING_FOR_ANSWER          =  0b1 << 3,
-//   CC_WAITING_FOR_PONG            =  0b1 << 4,
-// } CcWaitingFor;
-
 gboolean cc_comm_make_connection (CcComm  *comm,
                                   gchar   *remote_address,
                                   GError **error);
@@ -113,4 +74,4 @@ gboolean cc_comm_send_request (CcComm       *comm,
                                gchar        *utf8_payload,
                                GError      **error);
 
-G_END_DECLS // TODO: separate out cc stuff in a different header file
+G_END_DECLS
